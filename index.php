d913af3599e648b915b5a970ac1f849aa5d1208e
DamBer00593 <99778771+DamBer00593@users.noreply.github.com>
2023-03-28 01:00:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,66 @@
 <head>
-    <script src = "mainFuckSakesYouLittleBitch.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hangman A5</title>
+    <script><?php include('mainV6.js');?></script>
     <style><?php include('main.css'); ?></style>
 </head>
 <body>
 
     <header>
-        <div class = "Header">
         <h3>Hangman A5</h3>
+        <div id = "usernameField">
+            SampleUsername teehee
         </div>
     </header>
+
+    <div id="content">
+        <div id = "categoryButtons"></div>
+        <div id = "gameDiv" class = "hidden">
+            <div id = "images">
+                <img src = "Images/Hangman1.png" alt = "6 lives remaining" id = "Lives6">
+                <img src = "Images/Hangman2.png" alt = "5 lives remaining" class = "hidden" id = "Lives5">
+                <img src = "Images/Hangman3.png" alt = "4 lives remaining" class = "hidden" id = "Lives4">
+                <img src = "Images/Hangman4.png" alt = "3 lives remaining" class = "hidden" id = "Lives3">
+                <img src = "Images/Hangman5.png" alt = "2 lives remaining" class = "hidden" id = "Lives2">
+                <img src = "Images/Hangman6.png" alt = "1 lives remaining" class = "hidden" id = "Lives1">
+                <img src = "Images/Hangman7.png" alt = "0 lives remaining" class = "hidden" id = "Lives0">
+            </div>
+            <div id="letters"></div>
+            <div id = "leaderboard">
+                <table>
+                    <tr>
+                        <th>Username</th>
+                        <th>Score</th>
+                        <th>Category</th>
+                    </tr>
+                    <?php
+                        include 'dbConn.php';
+
+                        $conn = openCon();
+                        $sql = "SELECT UserName, max(Score) as Score, CategoryName 
+                        from GamesPlayed g join UserData u on u.UserID = g.GamePlayerID join Category c on c.CategoryID = g.CategoryID 
+                        group by UserName, CategoryName
+                        order by Score desc";
+
+                        $result = $conn->query($sql);
+                        if ($result->num_rows > 0) {
+                            while($row = $result->fetch_assoc()) {
+                                echo "<tr><td>" . $row["UserName"] . "</td><td>" . $row["Score"] . "</td><td>" . $row["CategoryName"] . "</td></tr>";
+                            }
+                        }
+                        CloseCon($conn);
+                    ?>
+                </table>
+            </div>
+
+            <div id="buttons"></div>
+            <button id = "playAgain">Play Again</button>
+        </div>
+        
+    </div>
+
     <div id = "Login" class = "loginPane">
         <div id = "login_Background">
             <div id = "login_Information">
@@ -39,4 +91,12 @@
             </div>
         </div>
     </div>
+
+    <footer>
+        <p>Assignment Group Members</p>
+        <ul>
+            <li>Hunter McQuinn</li>
+            <li>Damian Bernatchez</li>
+        </ul>
+    </footer>
 </body>
